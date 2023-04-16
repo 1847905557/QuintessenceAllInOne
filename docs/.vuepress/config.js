89d8334ed70312f87919a7773563158762612ae0
1847905557 <1847905557@qq.com>
2023-04-16 17:49:35 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: [],
+
+    themeConfig: {
+        sidebarDepth: 2,
+
+        sidebar: {
+            
+            '/': [
+
+                '',  //该目录下的README.md文件
+
+                
+                
+            ],
+        },
+    }
+}

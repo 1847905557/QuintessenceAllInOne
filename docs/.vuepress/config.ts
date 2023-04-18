56b155e35625843777e2cd29890e0656808d7b7b
1847905557 <1847905557@qq.com>
2023-04-19 04:54:11 +0800

    framework

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -24,27 +24,27 @@ export default defineUserConfig({
               collapsible: true,
               children: [
                 {
-                    text: 'chapter 1',
+                    text: 'chapter 1 intro',
                     link: '/section-1-sd/chapter-1-intro/',
                     children: [],
                 },
                 {
-                    text: 'chapter 2',
+                    text: 'chapter 2 prompt',
                     link: '/section-1-sd/chapter-2-prompt/',
                     children: [],
                 },
                 {
-                    text: 'chapter 3',
+                    text: 'chapter 3 model',
                     link: '/section-1-sd/chapter-3-model/',
                     children: [],
                 },
                 {
-                    text: 'chapter 4',
+                    text: 'chapter 4 training',
                     link: '/section-1-sd/chapter-4-training/',
                     children: [],
                 },
                 {
-                    text: 'chapter 5',
+                    text: 'chapter 5 tip',
                     link: '/section-1-sd/chapter-5-tip/',
                     children: [],
                 },
@@ -56,32 +56,32 @@ export default defineUserConfig({
                 collapsible: true,
                 children: [
                   {
-                      text: 'chapter 1',
+                      text: 'chapter 1 intro',
                       link: '/section-2-gpt/chapter-1-intro/',
                       children: [],
                   },
                   {
-                      text: 'chapter 2',
+                      text: 'chapter 2 basic',
                       link: '/section-2-gpt/chapter-2-basic/',
                       children: [],
                   },
                   {
-                      text: 'chapter 3',
+                      text: 'chapter 3 personality',
                       link: '/section-2-gpt/chapter-3-personality/',
                       children: [],
                   },
                   {
-                      text: 'chapter 4',
+                      text: 'chapter 4 creative',
                       link: '/section-2-gpt/chapter-4-creative/',
                       children: [],
                   },
                   {
-                      text: 'chapter 5',
+                      text: 'chapter 5 deployment',
                       link: '/section-2-gpt/chapter-5-deployment/',
                       children: [],
                   },
                   {
-                    text: 'chapter 6',
+                    text: 'chapter 6 tip',
                     link: '/section-2-gpt/chapter-6-tip/',
                     children: [],
                 },
@@ -93,27 +93,27 @@ export default defineUserConfig({
                 collapsible: true,
                 children: [
                   {
-                      text: 'chapter 1',
+                      text: 'chapter 1 intro',
                       link: '/section-3-aip/chapter-1-intro/',
                       children: [],
                   },
                   {
-                      text: 'chapter 2',
+                      text: 'chapter 2 photoshop',
                       link: '/section-3-aip/chapter-2-photoshop/',
                       children: [],
                   },
                   {
-                      text: 'chapter 3',
+                      text: 'chapter 3 blender',
                       link: '/section-3-aip/chapter-3-blender/',
                       children: [],
                   },
                   {
-                      text: 'chapter 4',
+                      text: 'chapter 4 bot',
                       link: '/section-3-aip/chapter-4-bot/',
                       children: [],
                   },
                   {
-                      text: 'chapter 5',
+                      text: 'chapter 5 gptuber',
                       link: '/section-3-aip/chapter-5-gptuber/',
                       children: [],
                   },
